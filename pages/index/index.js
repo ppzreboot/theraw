5ac2f347d258ba01caf3367498c1ca8cbe5b8939
PPz <ppzreboot@qq.com>
2024-06-11 11:49:29 +0800

    feat: 生命周期

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,4 +10,11 @@ Page({
     // 标签里的 data-* 可以在 event.target.dataset 中取到
     console.log(event.target.dataset.name)
   },
+  onLoad() {
+    console.log('loading')
+  },
+  onReady() {
+    // onLoad 和 onReady 是两个“生命周期毁掉函数”，其中 onReady 类似 Vue 里的 mounted
+    console.log('page ready')
+  },
 })
