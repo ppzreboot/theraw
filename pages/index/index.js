a1e23c8026372c2ddc6e1cea2d40cf29d9e6805b
PPz <ppzreboot@qq.com>
2024-06-11 11:49:02 +0800

    feat: 模板与事件

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,13 @@
 Page({
   data: {
-  }
+    userinfo: {
+      name: 'PPz',
+      year: 4,
+    }
+  },
+  gogogo(event) {
+    console.log('PPz, Go!')
+    // 标签里的 data-* 可以在 event.target.dataset 中取到
+    console.log(event.target.dataset.name)
+  },
 })
