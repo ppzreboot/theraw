caa9f87ec9138fcc839962c076fa259759efb249
PPz <ppzreboot@qq.com>
2024-06-15 10:39:55 +0800

    feat: choose address

--- pages/buy/form/index.js
+++ pages/buy/form/index.js
@@ -1,8 +1,16 @@
 Page({
   data: {
-
+    address: null,
+    formatted_tel: null,
   },
-  onShow() {
-
-  },
+  choose_address() {
+    wx.chooseAddress({
+      success: address =>
+        this.setData({
+          address,
+          formatted_tel: address.telNumber.slice(0, 3)
+            + 'xxxx' + address.telNumber.slice(-4)
+        })
+    })
+  }
 })
