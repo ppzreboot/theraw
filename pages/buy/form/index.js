932bdbb486d6ab724517368761f160e3a0d6a305
PPz <ppzreboot@qq.com>
2024-06-16 18:03:05 +0800

    fix: formatted name

--- pages/buy/form/index.js
+++ pages/buy/form/index.js
@@ -1,6 +1,7 @@
 Page({
   data: {
     address: null,
+    formatted_name: null,
     formatted_tel: null,
   },
   choose_address() {
@@ -8,6 +9,8 @@ Page({
       success: address =>
         this.setData({
           address,
+          formatted_name: address.userName.slice(0, 1)
+            + Array(address.userName.length - 1).fill('x').join(''),
           formatted_tel: address.telNumber.slice(0, 3)
             + 'xxxx' + address.telNumber.slice(-4)
         })
