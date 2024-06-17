c1be1b3f9282e6ae07d811e09f9a1d80a3da2549
PPz <ppzreboot@qq.com>
2024-06-17 16:36:21 +0800

    feat: seller's tap order

--- pages/order/index.js
+++ pages/order/index.js
@@ -1,3 +1,4 @@
+const { choose_action } = require("../../fp/util")
 const sell = require("../../services/sell")
 
 Page({
@@ -11,5 +12,17 @@ Page({
   },
   async onShow() {
     await this.load()
+  },
+
+  ontap_order(evt) {
+    const id = evt.currentTarget.dataset.id
+    const { order, product } = this.data.list.find(item => item.order._id == id)
+    choose_action([
+      ['拨打收货人电话', () => {
+        wx.makePhoneCall({ phoneNumber: order.last_address.telNumber })
+      }],
+      ['订单详情', '../info/index'],
+      ['更新订单状态', '../form/index'],
+    ])
   },
 })
